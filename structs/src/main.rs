ee52209a40003359009e24398e9735d08ebb80a7
rahul.lokurte <rahulmlokurte>
2022-10-24 00:10:42 +0530

    struct users

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -10,6 +10,27 @@ fn main() {
     println!("The active is {}", user1.active);
     println!("The email is {}", user1.email);
     println!("The sign_in_count is {}", user1.sign_in_count);
+    let s =  build_user(String::from("rahul.m.lokurte@gmail.com"), String::from("Rahul Lokurte"));
+    println!("The active is {}", s.sign_in_count);
+    let s =  build_user_short_hand(String::from("rahul.m.lokurte@gmail.com"), String::from("Rahul Lokurte"));
+    println!("The active is {}", s.sign_in_count);
+
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+
+        email: String::from("user2_rahul.m.lokurte@gmail.com"),
+        sign_in_count: user1.sign_in_count
+    };
+
+    println!("The email for user2 is {}", user2.email);
+
+    let user3 = User {
+        email: String::from("user3_rahul.m.lok"),
+        ..user2
+    };
+
+    println!("The email for user3 is {}", user3.email);
 }
 
 struct User {
@@ -17,4 +38,22 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn build_user_short_hand(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 2,
+    }
 }
